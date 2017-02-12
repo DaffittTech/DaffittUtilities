87d5e4df9e5c0e99b0f045bad4533c58418e6d6c
David Covey <CoveyNet@msn.com>
2017-02-12 16:47:17 -0600

    Rebuilt the entire Test project to get rid of the auto-generated IntelliTest classes. This allows us to do unit testing without dependency on the PexAssembly.

---
+++ DaffittMath/DaffittMath.Test/PrimeTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DaffittMath.Test
+{
+    [TestClass]
+    public class PrimeTest
+    {
+        // Test the result of passing NULL value to the IsPrime method
+        // This test should pass (return False) because NULL is NOT a prime number
+        [TestMethod]
+        public void IsPrime_Null()
+        {
+            bool b;
+            Prime p = new Prime();
+            b = p.IsPrime(default(int?));
+            Assert.AreEqual<bool>(false, b);
+            Assert.IsNotNull((object)p);
+        }
+
+        // Test the result of passing 0 to the IsPrime method
+        // This test should pass (return False) because 0 is NOT a prime number
+        [TestMethod]
+        public void IsPrime_0()
+        {
+            bool b;
+            Prime p = new Prime();
+            b = p.IsPrime(0);
+            Assert.AreEqual<bool>(false, b);
+            Assert.IsNotNull((object)p);
+        }
+
+        // Test the result of passing 1 to the IsPrime method
+        // This test should pass (return False) because 1 is NOT a prime number
+        [TestMethod]
+        public void IsPrime_1()
+        {
+            bool b;
+            Prime p = new Prime();
+            b = p.IsPrime(1);
+            Assert.AreEqual<bool>(false, b);
+            Assert.IsNotNull((object)p);
+        }
+
+        // Test the result of passing 2 to the IsPrime method
+        // This test should pass (return True) because 2 IS a prime number
+        [TestMethod]
+        public void IsPrime_2()
+        {
+            bool b;
+            Prime p = new Prime();
+            b = p.IsPrime(2);
+            Assert.AreEqual<bool>(true, b);
+            Assert.IsNotNull((object)p);
+        }
+
+        // Test the result of passing 7 to the IsPrime method
+        // This test should pass (return True) because 7 IS a prime number
+        [TestMethod]
+        public void IsPrime_7()
+        {
+            bool b;
+            Prime p = new Prime();
+            b = p.IsPrime(7);
+            Assert.AreEqual<bool>(true, b);
+            Assert.IsNotNull((object)p);
+        }
+
+        // Test the result of passing 9 to the IsPrime method
+        // This test should pass (return False) because 9 is NOT a prime number
+        [TestMethod]
+        public void IsPrime_9()
+        {
+            bool b;
+            Prime p = new Prime();
+            b = p.IsPrime(9);
+            Assert.AreEqual<bool>(false, b);
+            Assert.IsNotNull((object)p);
+        }
+    }
+}
